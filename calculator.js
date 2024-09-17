8da9936a17dee3e5d68b89c5710674fecc860196
EegArlert <bsyahputra@oakland.edu>
2024-09-16 21:23:00 -0400

    change some styling of the calculator. Created function to run calculator, setting the display as user input, and created calculate function

--- calculator.js
+++ calculator.js
@@ -1,41 +1,76 @@
-// console.log("hello world");
 
-function add(var1, var2) {
-    return var1 + var2;
-}
-
-function sub(var1, var2) {
-    return var1 - var2;
-}
-
-function mult(var1, var2) {
-    return var1 * var2;
-}
-
-function divd(var1, var2) {
-    return var1 / var2;
-}
-
-function operate(operation, var1, var2) {
-    if (operation == "add") {
-        add(var1, var2)
+function operate(var1, var2, operation) {
+    if (operation == "+") {
+        return var1 + var2
     }
-    else if (operation == "subtract") {
-        sub(var1, var2)
+    else if (operation == "-") {
+        return var1 - var2
     }
-    else if (operation == "multiply") {
-        mult(var1, var2)
+    else if (operation == "*") {
+        return var1 * var2
     }
-    else if (operation == "divide") {
-        divd(var1, var2)
+    else if (operation == "/") {
+        return var1 / var2
     }
 }
 
-const buttons = document.querySelectorAll(".btn-numerical");
+let num1 = null;
+let num2 = null;
+let operator = null;
+let nonOperator = null;
+let ans = 0;
 
-buttons.forEach(button => {
-    button.addEventListener("click", function() {
-        
+let buttons = document.querySelectorAll("button");
+const result = document.querySelector(".result");
+const onGoing = document.querySelector(".on-going-request");
+
+
+
+function runCalculator(){
+    
+    buttons.forEach(button => {
+        button.addEventListener("click", function() {
+            if(button.className == "numerical" && num1 == null){
+                num1 = parseInt(button.innerText);
+                onGoing.innerText = num1
+            }
+            else if(button.className == "numerical" && num2 == null && operator != null){
+                num2 = parseInt(button.innerText);
+                onGoing.innerText += " " + num2
+            }
+            else if(button.className == "operator" && num1 != null){
+                operator = button.innerText
+                onGoing.innerText += " " + operator
+            }
+            else if(button.className == "function"){
+                if (button.id == "btn-clear"){
+                    //empty out calculator
+                }
+                else if(button.id == "btn-enter"){
+                    num1 = calculate(num1, num2, operator);
+                    num2 = null
+                    operator = null
+                    result.innerText = num1
+                    onGoing.innerText = ""                    
+                }
+
+            }
+        })
     })
-})
+
+    
+
+}
+
+runCalculator();
+
+   
+
+function calculate(operand1, operand2, operator){
+    if(operator == null || operand1 == null || operand2 == null){
+        return null
+    }   
+    ans = operate(operand1, operand2, operator)
+    return ans;
+}
 
