cb5e8f59ee2487389b9291d8d5af8bebe6277ecc
EegArlert <bsyahputra@oakland.edu>
2024-09-17 09:12:30 -0400

    fix calculator logic to accept more than 2 digit number

--- calculator.js
+++ calculator.js
@@ -16,6 +16,8 @@ function operate(var1, var2, operation) {
 
 let num1 = null;
 let num2 = null;
+let stringNum1 = "";
+let stringNum2 = "";
 let operator = null;
 let nonOperator = null;
 let ans = 0;
@@ -30,28 +32,34 @@ function runCalculator(){
     
     buttons.forEach(button => {
         button.addEventListener("click", function() {
-            if(button.className == "numerical" && num1 == null){
-                num1 = parseInt(button.innerText);
-                onGoing.innerText = num1
+            if(button.className == "numerical" && num2 == null && operator == null){
+                stringNum1 += button.innerText
+                onGoing.innerText = stringNum1
+                num1 = Number(stringNum1)
             }
-            else if(button.className == "numerical" && num2 == null && operator != null){
-                num2 = parseInt(button.innerText);
-                onGoing.innerText += " " + num2
+            else if(button.className == "numerical" && num1 != null && operator != null){
+                stringNum2 += button.innerText
+                onGoing.innerText = stringNum1 + " " + operator + " " + stringNum2
+                num2 = Number(stringNum2)
+
+                //this doesn't display correctly
             }
             else if(button.className == "operator" && num1 != null){
                 operator = button.innerText
-                onGoing.innerText += " " + operator
+                onGoing.innerText += " " + operator;
+
             }
             else if(button.className == "function"){
                 if (button.id == "btn-clear"){
-                    //empty out calculator
+                    num1 = null
+                    num2 = null
                 }
                 else if(button.id == "btn-enter"){
                     num1 = calculate(num1, num2, operator);
                     num2 = null
                     operator = null
                     result.innerText = num1
-                    onGoing.innerText = ""                    
+                    onGoing.innerText = num1                   
                 }
 
             }
@@ -70,7 +78,6 @@ function calculate(operand1, operand2, operator){
     if(operator == null || operand1 == null || operand2 == null){
         return null
     }   
-    ans = operate(operand1, operand2, operator)
-    return ans;
+    return operate(operand1, operand2, operator)
 }
 
